aeeb36466066bdba3323517a7882fab573bc86c8
Malay-24 <malaybajpai24@gmailcom>
2023-02-19 14:23:24 +0530

    check

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ require("dotenv").config()
 const cors=require("cors")
 
 const app=express()
-app.use(cors({
-    origin:"*"
-}))
+app.use(cors())
 app.use(express.json())
 
 app.get("/",(req,res)=>{
